34370d8f0044059e46a91e73ae938e063af560e8
Marivaldo Junior <marivaldorjunior@gmail.com>
2015-07-11 22:49:56 -0300

    filtro para proteger paginas para acesso com login

---
+++ src/main/java/service/AuthorizationService.java
@@ -0,0 +1,47 @@
+package service;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.Usuario;
+
+
+public class AuthorizationService implements Filter{
+
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		
+		HttpSession session = ((HttpServletRequest)request).getSession();
+		Usuario usuario = (Usuario)session.getAttribute("usuario");
+		
+		if(usuario==null) {
+            session.setAttribute("notification_type", "danger");
+            session.setAttribute("notification_message", "Você precisa estar logado para visualizar esta página!");
+            String contextPath = ((HttpServletRequest)request).getContextPath();
+            ((HttpServletResponse)response).sendRedirect(contextPath+"/login.jsp");
+		}else{
+			chain.doFilter(request, response);
+		}
+		
+	}
+
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+}
